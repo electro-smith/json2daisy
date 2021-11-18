9096d1ea46c89664044dfa56701ed8f2eabcd5a3
CorvusPrudens <corvusprudens@gmail.com>
2021-11-18 15:44:17 -0700

    Fixed namespace issues

--- src/json2daisy/templates/daisy.h
+++ src/json2daisy/templates/daisy.h
@@ -1,13 +1,21 @@
+{{license}}
+
 #ifndef __JSON2DAISY_{{name|upper}}_H__
 #define __JSON2DAISY_{{name|upper}}_H__
 
 {% if som == 'seed' %}
 #include "daisy_seed.h"
+#include "dev/codec_ak4556.h"
 {% elif som == 'patch_sm' %}
 #include "daisy_patch_sm.h"
 {% endif %}
+{{display_conditional}}
+
+#define ANALOG_COUNT {{analogcount}}
 
 namespace json2daisy {
+
+{{non_class_declarations}}
 
 struct Daisy{{ name|capitalize }} {
 
@@ -92,16 +100,16 @@ struct Daisy{{ name|capitalize }} {
 
     {% if som == 'seed' and external_codecs|length > 0 %}
     // External Codec Initialization
-    SaiHandle::Config sai_config[{{ 1 + external_codecs|length }}];
+    daisy::SaiHandle::Config sai_config[{{ 1 + external_codecs|length }}];
 
     // Internal Codec
-    sai_config[0].periph          = SaiHandle::Config::Peripheral::SAI_1;
-    sai_config[0].sr              = SaiHandle::Config::SampleRate::SAI_48KHZ;
-    sai_config[0].bit_depth       = SaiHandle::Config::BitDepth::SAI_24BIT;
-    sai_config[0].a_sync          = SaiHandle::Config::Sync::MASTER;
-    sai_config[0].b_sync          = SaiHandle::Config::Sync::SLAVE;
-    sai_config[0].a_dir           = SaiHandle::Config::Direction::TRANSMIT;
-    sai_config[0].b_dir           = SaiHandle::Config::Direction::RECEIVE;
+    sai_config[0].periph          = daisy::SaiHandle::Config::Peripheral::SAI_1;
+    sai_config[0].sr              = daisy::SaiHandle::Config::SampleRate::SAI_48KHZ;
+    sai_config[0].bit_depth       = daisy::SaiHandle::Config::BitDepth::SAI_24BIT;
+    sai_config[0].a_sync          = daisy::SaiHandle::Config::Sync::MASTER;
+    sai_config[0].b_sync          = daisy::SaiHandle::Config::Sync::SLAVE;
+    sai_config[0].a_dir           = daisy::SaiHandle::Config::Direction::TRANSMIT;
+    sai_config[0].b_dir           = daisy::SaiHandle::Config::Direction::RECEIVE;
     sai_config[0].pin_config.fs   = {DSY_GPIOE, 4};
     sai_config[0].pin_config.mclk = {DSY_GPIOE, 2};
     sai_config[0].pin_config.sck  = {DSY_GPIOE, 5};
@@ -109,13 +117,13 @@ struct Daisy{{ name|capitalize }} {
     sai_config[0].pin_config.sb   = {DSY_GPIOE, 3};
 
     {% for codec in external_codecs %}
-    sai_config[{{loop.index}}].periph          = SaiHandle::Config::Peripheral::{{codec.periph}};
-    sai_config[{{loop.index}}].sr              = SaiHandle::Config::SampleRate::SAI_48KHZ;
-    sai_config[{{loop.index}}].bit_depth       = SaiHandle::Config::BitDepth::SAI_24BIT;
-    sai_config[{{loop.index}}].a_sync          = SaiHandle::Config::Sync::{{codec.a_sync}};
-    sai_config[{{loop.index}}].b_sync          = SaiHandle::Config::Sync::{{codec.b_sync}};
-    sai_config[{{loop.index}}].a_dir           = SaiHandle::Config::Direction::{{codec.a_dir}};
-    sai_config[{{loop.index}}].b_dir           = SaiHandle::Config::Direction::{{codec.b_dir}};
+    sai_config[{{loop.index}}].periph          = daisy::SaiHandle::Config::Peripheral::{{codec.periph}};
+    sai_config[{{loop.index}}].sr              = daisy::SaiHandle::Config::SampleRate::SAI_48KHZ;
+    sai_config[{{loop.index}}].bit_depth       = daisy::SaiHandle::Config::BitDepth::SAI_24BIT;
+    sai_config[{{loop.index}}].a_sync          = daisy::SaiHandle::Config::Sync::{{codec.a_sync}};
+    sai_config[{{loop.index}}].b_sync          = daisy::SaiHandle::Config::Sync::{{codec.b_sync}};
+    sai_config[{{loop.index}}].a_dir           = daisy::SaiHandle::Config::Direction::{{codec.a_dir}};
+    sai_config[{{loop.index}}].b_dir           = daisy::SaiHandle::Config::Direction::{{codec.b_dir}};
     sai_config[{{loop.index}}].pin_config.fs   = som.GetPin({{codec.pin.fs}});
     sai_config[{{loop.index}}].pin_config.mclk = som.GetPin({{codec.pin.mclk}});
     sai_config[{{loop.index}}].pin_config.sck  = som.GetPin({{codec.pin.sck}});
@@ -123,18 +131,18 @@ struct Daisy{{ name|capitalize }} {
     sai_config[{{loop.index}}].pin_config.sb   = som.GetPin({{codec.pin.sb}});
     {% endfor %}
 
-    SaiHandle sai_handle[{{ 1 + external_codecs|length }}];
+    daisy::SaiHandle sai_handle[{{ 1 + external_codecs|length }}];
     sai_handle[0].Init(sai_config[0]);
     {% for codec in external_codecs %}
     sai_handle[{{loop.index}}].Init(sai_config[{{loop.index}}]);
     {% endfor %}
 
     dsy_gpio_pin codec_reset_pin = som.GetPin(29);
-    Ak4556::Init(codec_reset_pin);
+    daisy::Ak4556::Init(codec_reset_pin);
 
-    AudioHandle::Config cfg;
+    daisy::AudioHandle::Config cfg;
     cfg.blocksize  = 48;
-    cfg.samplerate = SaiHandle::Config::SampleRate::SAI_48KHZ;
+    cfg.samplerate = daisy::SaiHandle::Config::SampleRate::SAI_48KHZ;
     cfg.postgain   = 0.5f;
     som.audio_handle.Init(
       cfg, 
@@ -169,17 +177,17 @@ struct Daisy{{ name|capitalize }} {
   void SetAudioSampleRate(size_t sample_rate) 
   {
     {% if som == 'seed' %}
-    SaiHandle::Config::SampleRate enum_rate;
+    daisy::SaiHandle::Config::SampleRate enum_rate;
     if (sample_rate >= 96000)
-      enum_rate = SAI_96KHZ;
+      enum_rate = daisy::SaiHandle::Config::SampleRate::SAI_96KHZ;
     else if (sample_rate >= 48000)
-      enum_rate = SAI_48KHZ;
+      enum_rate = daisy::SaiHandle::Config::SampleRate::SAI_48KHZ;
     else if (sample_rate >= 32000)
-      enum_rate = SAI_32KHZ;
+      enum_rate = daisy::SaiHandle::Config::SampleRate::SAI_32KHZ;
     else if (sample_rate >= 16000)
-      enum_rate = SAI_16KHZ;
+      enum_rate = daisy::SaiHandle::Config::SampleRate::SAI_16KHZ;
     else
-      enum_rate = SAI_8KHZ;
+      enum_rate = daisy::SaiHandle::Config::SampleRate::SAI_8KHZ;
     som.SetAudioSampleRate(enum_rate);
     {% elif som == 'patch_sm' %}
     som.SetAudioSampleRate(sample_rate);
@@ -198,7 +206,7 @@ struct Daisy{{ name|capitalize }} {
   /** Starts up the audio callback process with the given callback
    * 
    */
-  inline void StartAudio(AudioHandle::AudioCallback cb)
+  inline void StartAudio(daisy::AudioHandle::AudioCallback cb)
   {
     som.StartAudio(cb);
   }
@@ -206,6 +214,9 @@ struct Daisy{{ name|capitalize }} {
   /** This is the board's "System On Module"
    */
   {{som_class}} som;
+  {% if som == 'seed' %}
+  daisy::AdcChannelConfig cfg[ANALOG_COUNT];
+  {% endif %}
 
   // I/O Components
   {{comps}}
