0875fbd2a1f354121744688d284d08de6fa94f78
CorvusPrudens <corvusprudens@gmail.com>
2021-11-18 18:18:24 -0700

    Made import scheme simpler / more expected

--- src/json2daisy/__init__.py
+++ src/json2daisy/__init__.py
@@ -1,4 +1,1 @@
-from .json2daisy import generate_header_from_file
-from .json2daisy import generate_header_from_name
-
-__all__ = ['generate_header_from_file', 'generate_header_from_name']
+from .json2daisy import *
