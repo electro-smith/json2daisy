a92e1476982a04da8b36626d5fe76b29fca864f6
CorvusPrudens <corvusprudens@gmail.com>
2021-11-30 17:12:29 -0700

    Updated tests to account for new output format

--- tests/gen_integration_tests.py
+++ tests/gen_integration_tests.py
@@ -45,18 +45,18 @@ class TestIntegration(unittest.TestCase):
 test_template = """
     def test_{name}(self):
         self.maxDiff = None
-        header, name, components, aliases = json2daisy.generate_header_from_name('{name}')
+        header, info = json2daisy.generate_header_from_name('{name}')
         with open(path.join(data_path, 'integration', 'expected_{name}.h'), 'r') as file:
           self.assertEqual(header, file.read(), 'The output string should match "expected_{name}.h" exactly')
 """
 
 for template_name in included_json:
   if template_name not in non_templates:
-    header, name, components, aliases = json2daisy.generate_header_from_name(template_name)
-    template_test_path = path.join('integration', f'expected_{name}.h')
+    header, info = json2daisy.generate_header_from_name(template_name)
+    template_test_path = path.join('integration', f'expected_{info["name"]}.h')
     with open(path.join(data_path, template_test_path), 'w') as file:
       file.write(header)
-    test_file += test_template.format_map({'name': name})
+    test_file += test_template.format_map({'name': info['name']})
 
 with open(path.join(abs_path, 'integration_test.py'), 'w') as file:
   file.write(test_file)
--- tests/integration_test.py
+++ tests/integration_test.py
@@ -14,30 +14,30 @@ class TestIntegration(unittest.TestCase):
 
     def test_patch(self):
         self.maxDiff = None
-        header, name, components, aliases = json2daisy.generate_header_from_name('patch')
+        header, info = json2daisy.generate_header_from_name('patch')
         with open(path.join(data_path, 'integration', 'expected_patch.h'), 'r') as file:
           self.assertEqual(header, file.read(), 'The output string should match "expected_patch.h" exactly')
 
     def test_patch_init(self):
         self.maxDiff = None
-        header, name, components, aliases = json2daisy.generate_header_from_name('patch_init')
+        header, info = json2daisy.generate_header_from_name('patch_init')
         with open(path.join(data_path, 'integration', 'expected_patch_init.h'), 'r') as file:
           self.assertEqual(header, file.read(), 'The output string should match "expected_patch_init.h" exactly')
 
     def test_petal(self):
         self.maxDiff = None
-        header, name, components, aliases = json2daisy.generate_header_from_name('petal')
+        header, info = json2daisy.generate_header_from_name('petal')
         with open(path.join(data_path, 'integration', 'expected_petal.h'), 'r') as file:
           self.assertEqual(header, file.read(), 'The output string should match "expected_petal.h" exactly')
 
     def test_pod(self):
         self.maxDiff = None
-        header, name, components, aliases = json2daisy.generate_header_from_name('pod')
+        header, info = json2daisy.generate_header_from_name('pod')
         with open(path.join(data_path, 'integration', 'expected_pod.h'), 'r') as file:
           self.assertEqual(header, file.read(), 'The output string should match "expected_pod.h" exactly')
 
     def test_field(self):
         self.maxDiff = None
-        header, name, components, aliases = json2daisy.generate_header_from_name('field')
+        header, info = json2daisy.generate_header_from_name('field')
         with open(path.join(data_path, 'integration', 'expected_field.h'), 'r') as file:
           self.assertEqual(header, file.read(), 'The output string should match "expected_field.h" exactly')
